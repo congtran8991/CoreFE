d0d728b588b055a530608b0e23fcb5a85638e488
CongTraanApollogix <frontend_dev@apollogix.net>
2026-04-13 22:39:20 +0700

    s

--- src/page/GroupDatasets/index.tsx
+++ src/page/GroupDatasets/index.tsx
@@ -152,7 +152,7 @@ const ListGroupDataset = () => {
     <>
       <KContainer.Card
         header={{
-          title: "Group 177hhhne",
+          title: "Group 177hhddhne",
           border: true,
           color: "#cb8609",
           bgColorIcon: "#cb8609",
